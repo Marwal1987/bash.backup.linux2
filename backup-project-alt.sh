d217a9b44a6effb2b13bb34e7344b6172e2b00a4
Marwal1987 <73877564+Marwal1987@users.noreply.github.com>
2020-11-14 14:00:25 +0100

    Update backup-project-alt.sh

--- backup-project-alt.sh
+++ backup-project-alt.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
-# backup-project-alt.sh
+# backupT5.sh
 
 # Backup to NFS mount script.
 # Encryption using openssl.
 
 # What to backup.
-backup_files="/root /etc /usr"
+backup_files="/home /root /etc /usr"
 
 # Where to backup to.
 dest="/mnt/backup"
 
 # Create archive filename.
-day=$(date +%A)                         # +%A means show current weekday.
-hostname=$(hostname -s)                 # -s  means shortversion of hostname. Cut at first dot.
+day=$(date +%A)                                                       # +%A means show current weekday.
+hostname=$(hostname -s)                                               # -s  means shortversion of hostname. Cut at first dot.
 archive_file="$hostname-$day.tgz"              
 
 # Print start status msg.
 echo "Backing up $backup_files to $dest/$archive_file"
 echo
 
-# Copy script to cron daily.
-# sudo cp backup-project-alt.sh /etc/cron.daily/backup-project-alt.sh
 
 # Check to see if the script exists in daily cron directory
 function check_schedule {
-       if [ ! -s "/etc/cron.daily/backup-project-alt" ]
-       then
-          # Copy script to cron.daily dir
-          sudo cp backup-project-alt.sh /etc/cron.daily/backup-project-alt            #Why sudo? The Cron directory is owned by root.
-          echo "The backup has been set to run daily"
-          echo "The exact run time is in the /etc/crontab file."
-          exit 1
-       fi
+  if [ ! -s "/etc/cron.daily/backupT5.sh" ]                           # -s means files exists and its size is greater than 0                
+  then 
+   sudo cp backupT5.sh /etc/cron.daily/backupT5.sh                    # Why sudo? The Cron directory is owned by root.
+   echo "The backup has been set to run daily"
+   echo "The exact run time is in the /etc/crontab file."
+  exit 1
+  fi
 }
 
 check_schedule
 
 # Backup the files with Tar and encrypt with openssl.
-tar czf $dest/"$archive_file" "$backup_files" | openssl enc -e -aes256 -out backup_secured.tar.gz
+tar czf $dest/"$archive_file" "$backup_files" | openssl enc -e -aes256 -out "$archive_file".tar.gz
 
 # Print end status msg.
 echo
@@ -46,7 +43,7 @@ echo "Backup finished."
 echo
 echo "Cleaning files older than 3 days."
 echo
-find $dest -type f -mtime +3 -exec rm -f {} \;
+find $dest -type f -mtime +3 -exec rm -fr {} \;                        # 
 
 echo
 echo "Cleaning finished."
