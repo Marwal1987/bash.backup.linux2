b0ac2b22113bee773eedf3e2ec0f580a55c3b058
maxgyl <74200970+maxgyl@users.noreply.github.com>
2020-11-17 16:55:22 +0100

    Update backuprestore.sh

--- backuprestore.sh
+++ backuprestore.sh
@@ -13,37 +13,37 @@ read WEEKDAY
 case $WEEKDAY in
 
   monday | Monday | MONDAY)
-    tar -xzvf $dest/"$hostname"-Monday* -C /
+    tar -xzvf $dest/"$hostname"-Monday.tgz -C /
     echo -n "Monday\'s backup has been restored"
     ;;
 
   tuesday | Tuesday | TUESDAY)
-    tar -xzvf $dest/"$hostname"-Tuesday* -C /
+    tar -xzvf $dest/"$hostname"-Tuesday.tgz -C /
     echo -n "Tuesday\'s backup has been restored"
     ;;
 
   wednesday | Wednesday | WEDNESDAY)
-    tar -xzvf $dest/"$hostname"-Wednesday* -C /
+    tar -xzvf $dest/"$hostname"-Wednesday.tgz -C /
     echo -n "Wednesday\'s backup has been restored"
     ;;
 
   thursday | Thursday | THURSDAY)
-    tar -xzvf $dest/"$hostname"-Thursday* -C /
+    tar -xzvf $dest/"$hostname"-Thursday.tgz -C /
     echo -n "Thursday\'s backup has been restored"
     ;;
     
   friday | Friday | FRIDAY)
-    tar -xzvf $dest/"$hostname"-Friday* -C /
+    tar -xzvf $dest/"$hostname"-Friday.tgz -C /
     echo -n "Thursday\'s backup has been restored"
     ;; 
     
   saturday | Saturday | SATURDAY)
-    tar -xzvf $dest/"$hostname"-Saturday* -C /
+    tar -xzvf $dest/"$hostname"-Saturday.tgz -C /
     echo -n "Saturday\'s backup has been restored"
     ;;
     
   sunday | Sunday | SUNDAY)
-    tar -xzvf $dest/"$hostname"-Sunday* -C /
+    tar -xzvf $dest/"$hostname"-Sunday.tgz -C /
     echo -n "Sunday\'s backup has been restored"
     ;;
 esac
